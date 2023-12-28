f630f46d432ab5bac56d316109975dc6d77fccf8
Mr.VACO <brute1904@gmail.com>
2023-12-29 00:06:54 +0500

    Added migrations

---
+++ migrations/create_blog_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use MrVaco\OrchidStatusesManager\Classes\StatusClass;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('mr_vaco__blog_categories', function(Blueprint $table)
+        {
+            $table->id();
+
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('keywords')->nullable();
+            $table->string('tags')->nullable();
+            $table->longText('description')->nullable();
+            $table->unsignedInteger('status')->default(StatusClass::DRAFT()->id);
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('creator_id')->unsigned();
+            $table->unsignedBigInteger('updator_id')->unsigned();
+            $table->boolean('hidden')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('mr_vaco__blog_categories');
+    }
+};
--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -2,9 +2,29 @@
 
 namespace MrVaco\OrchidBlog;
 
-use Illuminate\Support\ServiceProvider;
+use MrVaco\HelperCode\Classes\Migrations;
+use Orchid\Platform\Dashboard;
+use Orchid\Platform\OrchidServiceProvider;
 
-class BlogServiceProvider extends ServiceProvider
+class BlogServiceProvider extends OrchidServiceProvider
 {
-    public function boot(): void {}
+    public function boot(Dashboard $dashboard): void
+    {
+        parent::boot($dashboard);
+
+        $this->publish();
+    }
+
+    protected function publish(): void
+    {
+        if (!$this->app->runningInConsole())
+        {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__ . '/../migrations/create_blog_posts_table.php'      => Migrations::getMigrationFileName('create_blog_posts_table.php'),
+            __DIR__ . '/../migrations/create_blog_categories_table.php' => Migrations::getMigrationFileName('create_blog_categories_table.php'),
+        ], 'plugin-migrations');
+    }
 }
